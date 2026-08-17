b812d6190cd02357dfa19fa63bb0faee401e76f8
root <root@linux7703.cloudnest.vn>
2026-08-17 07:20:38 +0000

    v1.1.0

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
  * Beplus Test Theme - index
  */
 get_header(); ?>
-<main><h1>Beplus Test Theme</h1><p>Version 1.0.0</p></main>
+<main><h1>Beplus Test Theme</h1><p>Version 1.1.0</p></main>
 <?php get_footer();
